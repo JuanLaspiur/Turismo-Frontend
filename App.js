3b180ae86959b91e8ac0d44b89b5ec765e63adb5
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-08-07 18:26:48 -0300

    [Instalación de 'react-native-maps',  MapView, {Marker, Polyline} funcionan correctamente. Instalación de react-native-maps-directions, MapViewDirections sin probar funcionamiento por falta de ApiKey y cuenta Gmail ]

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function App() {
+  const [origin, setOrigin] = useState({
+    latitude: 41.40338,
+    longitude: 2.17403,
+  });
+  const [destination, setDestination] = useState({
+    latitude: 41.40338,
+    longitude: 2.17993,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      >
+        <Marker
+          draggable
+          coordinate={origin}
+          onDragEnd={(direction) => {
+            setOrigin(direction.nativeEvent.coordinate);
+          }}
+        />
+        <Marker
+          draggable
+          coordinate={destination}
+          onDragEnd={(direction) => {
+            setDestination(direction.nativeEvent.coordinate);
+          }}
+        />
+     {/*   <Polyline
+          coordinates={[origin, destination]}
+          strokeColor="pink"
+          strokeWidth={10}
+        /> */}
+
+        {/* 
+   <MapViewDirections
+      origin={origin}
+      destination{destination}
+      apikey={env.apiKey}
+       strokeColor="pink"
+       strokeWidth={20}
+    /> */}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +60,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  map: {
+    height: "100%",
+    width: "100%",
   },
 });
